889b2253f2438b0f81d4972b404add67a1b307aa
Chclt <meakingchan@gmail.com>
2023-07-29 20:54:04 +0800

    build

--- scene-ui.js
+++ scene-ui.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.body.addEventListener('sceneraisin', (event) => {
-        console.log('sceneraisin', event.detail);
         const raisin = document.createElement('scene-raisin');
         raisin.text = event.detail.text;
         document.body.appendChild(raisin);
